1244a5b835901b045dbb84bbc9b144def2f5f8d7
20191777 <97873629+20191777@users.noreply.github.com>
2023-02-08 20:19:42 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,5 +1,10 @@
 word = input()
-low_alpahbet = list(range(97,123))  # 아스키코드 숫자 범위
 
-for x in low_alpahbet :
-    print(word.find(chr(x))) 
+ans = []
+
+low_alpahbet = list(range(97,123))
+
+#for i in ascii_lowercase:
+for i in low_alpahbet:
+    idx = word.find(chr(i))
+    print(idx, end=" ")
