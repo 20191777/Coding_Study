34c9cbe672b2e670806aa912a2b314c1f72090c8
20191777 <97873629+20191777@users.noreply.github.com>
2023-08-21 12:27:25 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+while True :
+    s = input().rstrip()
+    if s == ".":
+        break
+    stack = []
+    for i in s :
+        if i == '[' or i == '(' :
+            stack.append(i)
+        elif i == ']' :
+            if len(stack) != 0 and stack[-1] == '[' :
+                stack.pop() # 맞으면 지워서 stack을 비워줌 0 = yes
+            else :
+                stack.append(']')
+                break
+        elif i == ')' :
+            if len(stack) != 0 and stack[-1] == '(' :
+                stack.pop()
+            else :
+                stack.append(')')
+                break
+    if len(stack) == 0 :
+        print('yes')
+    else :
+        print('no')
