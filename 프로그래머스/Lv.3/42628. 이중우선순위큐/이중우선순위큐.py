7df490c3552b8077d8776ce2bba605999f3ce9a1
20191777 <97873629+20191777@users.noreply.github.com>
2024-01-16 00:08:07 +0900

    [level Lv.3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,19 @@
+import heapq
+
+def solution(operations):
+    answer = []
+    heap = []
+    for oper in operations:
+        if oper[0] == "I":
+            heapq.heappush(heap, int(oper[2:]))
+        elif oper[0] == "D" and oper[2:] == "1" and len(heap) > 0:
+            heap.pop()
+        elif oper[0] == "D" and oper[2:] == "-1" and len(heap) > 0:
+            heapq.heappop(heap)
+        #print(heap)
+    if len(heap) == 0:
+        answer = [0, 0]
+    else:
+        answer.append(max(heap))
+        answer.append(heapq.heappop(heap))
+    return answer
